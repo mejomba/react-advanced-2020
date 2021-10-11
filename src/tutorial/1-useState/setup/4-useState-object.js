e92c8e62bea665822454784bbeac085d358ddd09
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2021-10-11 23:49:57 +0330

    x

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -2,15 +2,23 @@ import React, { useState } from 'react';
 import {data} from '../../../data';
 
 const UseStateObject = () => {
-const [person, setPerson] = useState({name: 'mojtaba', age: 25, 'message': 'hello'})
+// const [person, setPerson] = useState({name: 'mojtaba', age: 25, message: 'hello'})
+
+const [name, setName] = useState('mejomba');
+const [age, setAge] = useState(24);
+const [message, setMessage] = useState('hello mojtaba');
+
 const changeBtn = ()=> {
-  setPerson({...person, message: 'hello mojtaba'});
+  // setPerson({...person, message: 'hello mojtaba'});
+  setName('xxx');
+  setAge('yyy');
+  setMessage('zzz');
 }
 return <>
-  <h2>{person.name}</h2>
-  <h2>{person.age}</h2>
-  <h2>{person.message}</h2>
-  <button onClick={changeBtn}>change</button>
+  <h2>{name}</h2>
+  <h2>{age}</h2>
+  <h2>{message}</h2>
+  <button className='btn' onClick={changeBtn}>change</button>
   </>;
 };
 
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [count, setCount] = useState(0);
+  const reset = ()=> setCount(0);
+  const complexBtn = ()=> {
+    setTimeout(()=> {
+      setCount((old)=> {
+        return old + 1;
+      });
+    }, 2000);
+  }
+  return <>
+  <h2>simple counter</h2>
+  <h1>{count}</h1>
+  <button className='btn' onClick={()=> setCount(count - 1)}>decrease</button>
+  <button className='btn' onClick={reset}>reset</button>
+  <button className='btn' onClick={()=> setCount(count + 1)}>increase</button>
+  
+  {/* <h2>complex counter</h2> */}
+  {/* <h1>{count}</h1> */}
+  <button className='btn' onClick={complexBtn}>increase with delay</button>
+  </>;
 };
 
 export default UseStateCounter;
