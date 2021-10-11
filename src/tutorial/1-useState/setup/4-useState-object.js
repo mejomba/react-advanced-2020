baf9139c6f7b31d50e0c8d5bdda20296764c8cae
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2021-10-11 22:52:27 +0330

    x

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,8 +1,16 @@
 import React, { useState } from 'react';
+import {data} from '../../../data';
 
 const UseStateObject = () => {
-  return <>
-  
+const [person, setPerson] = useState({name: 'mojtaba', age: 25, 'message': 'hello'})
+const changeBtn = ()=> {
+  setPerson({...person, message: 'hello mojtaba'});
+}
+return <>
+  <h2>{person.name}</h2>
+  <h2>{person.age}</h2>
+  <h2>{person.message}</h2>
+  <button onClick={changeBtn}>change</button>
   </>;
 };
 
