4734435ad36d3cf4ed5f0d1755a3daae310289d3
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2021-10-25 12:45:39 +0330

    m

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -5,13 +5,17 @@ import React, { useState } from 'react';
 const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
-  const [text, settext] = useState('true')
+  // const [text, settext] = useState('true')
+  const [isError, setisError] = useState(false)
   return <>
   {/* {text || 'hello'} */}
   {/* {text && 'by'} */}
 
-  {text || <h2>if false</h2>}
-  {text && <h2>if true</h2>}
+  {/* {text || <h2>if false</h2>} */}
+  {/* {text && <h2>if true</h2>} */}
+  <button className='btn' onClick={()=> {setisError(!isError)}}>toggle error</button>
+  
+  {isError ? <h2>خطا رخ داد</h2> : <h2>خطا نداریم</h2>}
   </>;
 };
 
--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,33 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+const [showHide, setshowHide] = useState(false)
+
+  const show = ()=> {
+    setshowHide(!showHide)
+  }
+  return <>
+  <button className='btn' onClick={show}>ShowHide</button>
+  {showHide ? <Other /> : ''}
+  </>;
 };
+
+const Other = ()=> {
+  const [size, setsize] = useState(window.innerWidth)
+  const checkSize = ()=>{
+    setsize(window.innerWidth)
+  }
+  useEffect(()=>{
+  window.addEventListener('resize', checkSize)
+return ()=>{
+  window.removeEventListener('resize', checkSize)
+}
+},[])
+
+  return <>
+    <h2>other component</h2>
+    <h3>window: {size}</h3>
+  </>
+}
 
 export default ShowHide;
